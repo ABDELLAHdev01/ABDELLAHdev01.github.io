a1566feab61b88215ec20f65c0e5c8b642b80438
Abdellah EL GHOULAM <abdellahelghoulamdev@gmail.com>
2023-07-17 19:39:17 +0100

    update:adding a certificate to my portfolio

--- js/main.js
+++ js/main.js
@@ -45,16 +45,16 @@
     $(document).ready(function () {
         var $videoSrc;
         $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
+            // $videoSrc = $(this).data("src");
         });
-        console.log($videoSrc);
+        // console.log($videoSrc);
 
         $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+            // $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
         })
 
         $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
+            // $("#video").attr('src', $videoSrc);
         })
     });
 
